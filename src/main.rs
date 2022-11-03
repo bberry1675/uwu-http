7246acc0fdbe34828c9496d9c7cda98c49be59b0
Brandon Berry <brandon.jn.berry@gmail.com>
2022-11-03 00:44:23 +0000

    same as last commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use tiny_http::{Server, Response};
+use uwuifier::uwuify_str_sse;
+
 fn main() {
-    println!("Hello, world!");
+    let server = Server::http("0.0.0.0:8000").unwrap();
+    for request in server.incoming_requests() {
+        println!("received request! method: {:?}, url: {:?}, headers: {:?}",
+            request.method(),
+            request.url(),
+            request.headers()
+        );
+    
+        let response = Response::from_string(uwuify_str_sse("We cannot solve problems with the kind of thinking we employed when we came up with them - Albert Einstein"));
+        request.respond(response);
+    }
 }
